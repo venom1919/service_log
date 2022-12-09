757922db222057b52f26f3458da7313beb12e96f
Venom <lyaskivskii@gmail.com>
2022-12-09 17:35:16 +0200

    added rommDb

--- app/src/main/java/com/service_log/db/TripDB.kt
+++ app/src/main/java/com/service_log/db/TripDB.kt
@@ -1,12 +1,29 @@
 package com.service_log.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.service_log.dao.TripDao
 import com.service_log.model.Trip
 
 @Database(entities = [Trip::class], version = 1, exportSchema = false)
 abstract class TripDB : RoomDatabase() {
 
+        abstract fun abstractTrip() : TripDao
 
+        companion object{
+                private var INSTANCE: TripDB? = null
+                fun getInstance(context: Context): TripDB{
+                        if (INSTANCE == null){
+                                INSTANCE = Room.databaseBuilder(
+                                        context,
+                                        TripDB::class.java,
+                                        "TripService.db")
+                                        .build()
+                        }
 
+                        return INSTANCE as TripDB
+                }
+        }
 }
